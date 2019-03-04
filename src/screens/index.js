1b946f25eaa7e782f9057156284daf3bf6bce564
mohamed shawky <shawkymohamed138@gmail.com>
2019-03-04 00:43:13 -0800

    shaw

--- src/screens/index.js
+++ src/screens/index.js
@@ -5,26 +5,12 @@ import { Provider } from 'react-redux';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import store from '../store';
 
-
-import FindRest from './FindRest';
-import LocationMap from './mapScreen/LocationMap';
 import ProductList from './productList/ProductList';;
 import ProducDetails from "./productDetails/ProducDetails"
 import ShoppingCart from "./shoppingCart/ShoppingCart";
 
 export const registerScreens = () => {
-  Navigation.registerComponentWithRedux(
-    'FindRest',
-    () => gestureHandlerRootHOC(FindRest),
-    Provider,
-    store,
-  );
-  Navigation.registerComponentWithRedux(
-    'LocationMap',
-    () => gestureHandlerRootHOC(LocationMap),
-    Provider,
-    store,
-  );
+ 
   Navigation.registerComponentWithRedux(
     'productList',
     () => gestureHandlerRootHOC(ProductList),
